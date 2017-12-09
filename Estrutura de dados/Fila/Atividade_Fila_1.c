21fa77f4dee4d78b4d2671daa3e79d4dd785040b
Junior Pinheiro <paladinoshot32@gmail.com>
2017-12-09 17:19:26 -0300

    Update Atividade_Fila_1.c

--- Estrutura de dados/Fila/Atividade_Fila_1.c
+++ Estrutura de dados/Fila/Atividade_Fila_1.c
@@ -14,9 +14,9 @@ typedef struct {
 
 int menu (){
 	printf ("\n<<<<<<<<<|MENU|>>>>>>>>>\n");
-	
+
 	int r;
-	
+
 	printf ("0 – SAIR");
 	printf ("\n1 – INSERIR NA FILA");
 	printf ("\n2 – EXCLUIR DA FILA");
@@ -25,72 +25,89 @@ int menu (){
 	printf ("\n5 – MOSTRA O PRIMEIRO ELEMENTO DA FILA");
 	printf ("\nDigite sua opção: ");
 	scanf ("%d",&r);
-	
+
 	return r;
 }
 
 void startfila (fila *f){
 	f->inicio = 0;
-	f->fim = -1;		
+	f->fim = -1;
+}
+
+void fakefull (fila *f){
+    int auxinicio = f->inicio, auxfim = f->fim, cont;
+
+    if(f->fim == tam-1 && f->inicio != 0){
+        f->fim = -1;
+        f->inicio = 0;
+
+        for(cont = auxinicio; cont <= auxfim; cont++){
+            f->fim++;
+            f->matricula[f->fim] = f->matricula[cont];
+            f->trab[f->fim] = f->trab[cont];
+            f->prova[f->fim] = f->prova[cont];
+        }
+    }
 }
 
 
 void insertfila (fila *f){ //1
 	float n_prova, n_trab;
 	int matricula;
-	
+
 	printf ("\n<<<<<<<<<|INSERIR NA FILA|>>>>>>>>>\n");
-	
-	printf ("\nUSER VÍRGULA \",\" AO INVÉS DE PONTO \".\"\n");	
+
+	printf ("\nUSER VÍRGULA \",\" AO INVÉS DE PONTO \".\"\n");
 	if (f->fim == tam-1){
 		printf ("ERROR - FILA ESTÁ CHEIA.\n");
-	}else{		
+	}else{
 		printf ("\nDigite a matricula: ");
 		scanf ("%d",&matricula);
-		
+
 		printf ("\nDigite a nota da prova: ");
 		scanf ("%f",&n_prova);
-		
+
 		printf ("\nDigite a nota do trabalho: ");
-		scanf ("%f",&n_trab);	
-		
-		f->fim++;		
-		
+		scanf ("%f",&n_trab);
+
+		f->fim++;
+
 		f->matricula[f->fim] = matricula;
 		f->prova[f->fim] = n_prova;
-		f->trab[f->fim] = n_trab; 
+		f->trab[f->fim] = n_trab;
 	}
 	system("pause");
 }
 
 
 void removefila (fila *f){ //2
-	
+
 	printf ("\n<<<<<<<<<|REMOVER FILA|>>>>>>>>>\n");
-	
+
 	if (f->fim < f->inicio){
 		printf ("\nERROR - FILA VAZIA\n");
 	}else{
 		f->inicio++;
 	}
+	printf("\nREMOVIDO COM SUCESSO!\n");
 	system("pause");
 }
 
 
 void showfila (fila *f){ //3
 	printf ("\n<<<<<<<<<|MOSTRAR FILA|>>>>>>>>>\n");
-	
+
 	int cont;
-	
+
 	if (f->fim < f->inicio){
 		printf ("\nERROR - FILA VAZIA\n");
 	}
-	for (cont = f->inicio; cont < f->fim+1; cont++){		
+	for (cont = f->inicio; cont < f->fim+1; cont++){
 		printf ("\n\nMatricula: %d ",f->matricula[cont]);
 		printf ("\nNota da prova: %0.2f",f->prova[cont]);
 		printf ("\nNota do trabalho: %0.2f",f->trab[cont]);
 		printf ("\nMédia: %0.2f",(f->prova[cont]+f->trab[cont])/2);
-		
+
 		if ((f->prova[cont]+f->trab[cont])/2 >= 7.0){
 			printf ("\nAPROVADO");
 		}else{
@@ -101,44 +118,44 @@ void showfila (fila *f){ //3
 }
 
 
-void bignote (fila *f){ //4
+void bigger(fila *f){ //4
 	printf ("\n<<<<<<<<<|MAIOR NOTA|>>>>>>>>>\n");
-	
+
 	int cont;
 	float media[tam];
-	
+	float maior;
+
 	if (f->fim < f->inicio){
 		printf ("\nERROR - FILA VAZIA\n");
 	}else {
 		for (cont = f->inicio; cont < f->fim+1; cont++){
 			media[cont] = (f->prova[cont]+f->trab[cont])/2;
-		}
-	
-		float maior = media[0];
-	
+        }
+
+		maior = media[0];
 		for (cont = f->inicio; cont < f->fim+1; cont++){
 			if (maior < media[cont]){
-				maior = media[cont];	
+				maior = media[cont];
 			}
 		}
 		printf ("\nA maior nota é: %0.2f\n", maior);
-	}	
-	system("pause");	
+	}
+	system("pause");
 }
 
 
-void showtop (fila *f){ //5
+void showfrist (fila *f){ //5
 	printf ("\n<<<<<<<<<|ELEMENTO DO TOPO|>>>>>>>>>\n");
 	int x;
 	float auxp;
 	float auxt;
-	
+
 	if (f->fim < f->inicio){
 		printf ("\nERROR - FILA VAZIA\n");
 	}else {
 		x = f->matricula[f->inicio];
 		auxp = f->prova[f->inicio];
-		auxt = f->trab[f->inicio];		
+		auxt = f->trab[f->inicio];
 		printf ("\nMatricula: %d, Prova: %0.2f, Nota do trabalho: %0.2f.\n",x,auxp,auxt);
 	}
 	system("pause");
@@ -148,21 +165,22 @@ void showtop (fila *f){ //5
 main ()	{
 
 	setlocale(LC_ALL,"portuguese_Brazil");
-	
+
 	fila f;
-	
+
 	int r;
-	
+
 	startfila(&f);
-	
+
 	r = menu ();
-	
+
 	while (r != 0){
-		
+
 		if (r == 1 ){
 			system ("cls");
-			insertfila (&f);
-			system ("cls");	
+			fakefull(&f);
+			insertfila(&f);
+			system ("cls");
 		}
 		else if (r == 2){
 			system ("cls");
@@ -171,23 +189,23 @@ main ()	{
 		}
 		else if (r == 3){
 			system ("cls");
-			showfila (&f);
+			showfila(&f);
 			system ("cls");
 		}
 		else if (r == 4){
 			system ("cls");
-			bignote (&f);
+			bigger(&f);
 			system ("cls");
 		}
 		else if (r == 5){
 			system ("cls");
-			showtop (&f);
+			showfrist(&f);
 			system ("cls");
 		}
-		
-		r = menu ();		
+
+		r = menu ();
 	}
-	
-	
+
+
 	return 0;
 }
