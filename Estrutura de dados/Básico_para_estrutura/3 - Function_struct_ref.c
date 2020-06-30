d23114bd730088f212c5f85519b4a431505d1fbd
Junior Pinheiro <paladinoshot32@gmail.com>
2020-06-30 12:46:32 -0300

    passando struct para função por referencia

---
+++ Estrutura de dados/Básico_para_estrutura/3 - Function_struct_ref.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+// isso aqui é daora, passar struct por referência;
+//ISSO POSSIBLITA RETORNAR VÁRIOS VALORES DE UMA FUNCTION;
+
+struct ponto
+{
+  int x,y,z;
+};
+
+void atribui(struct ponto *p){ //precisa definir "struct tipo *nome"
+  (*p).x = 10;
+  (*p).y = 10;
+  (*p).z = (*p).x + (*p).y;
+  // É IMPORTANTE USAR O () NO *p POIS VC ESTÁ PASSANDO A STRUCT TODA POR REFERÊNCIA E NÃO UM ATRIBUTO DELA, SE NÃO USAR O COMPILADOR FICARÁ CONFUSO .
+  // E PENSARÁ QUE É PARA USAR (*p.y) AO INVÉS DE (*p).y;
+}
+
+
+void main(){
+  struct ponto p1;
+  atribui(&p1); //IMPORTANE PASSSAR COM & POR É POR REFERÊNCIA.
+  printf("valor de x: %d\n", p1.x);
+  printf("valor de y: %d\n", p1.y);
+  printf("valor de z: %d\n", p1.z);
+}
