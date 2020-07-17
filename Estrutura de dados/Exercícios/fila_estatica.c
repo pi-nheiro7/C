777f1e66615f3c87b34a3c868976694864cbfbf0
Junior Pinheiro <paladinoshot32@gmail.com>
2020-07-17 17:30:36 -0300

    fila completa

--- Estrutura de dados/Exercícios/fila_estatica.c
+++ Estrutura de dados/Exercícios/fila_estatica.c
@@ -20,6 +20,7 @@ void iniciarFila(fila *f){
 }
 
 int tamanhoFila(fila *f){
+  printf("Números de elementos: %d\n", f->NroElem);
   return f->NroElem;
 }
 
@@ -49,5 +50,75 @@ bool inserir(fila *f){
   int posicao = (f->inicio + f->NroElem) % max;
   f->reg[posicao] = newValor;
   f->NroElem++;
-  f->
+  printf("Valor inserido com sucesso!\n");
+  return true;
+}
+
+bool deletar(fila *f){
+  if(f->NroElem == 0){
+    printf("A pilha está vazia!\n");
+    return false;
+  }
+  f->inicio = (f->inicio+1)%max;
+  f->NroElem--;
+  return true;
+}
+
+void resetFila(fila *f){
+  f->inicio = 0;
+  f->NroElem = 0;
+}
+
+int menu()
+{
+  int r;
+  printf("0 - Stop");
+  printf("\n1 - Tamanho da Fila");
+  printf("\n2 - Mostrar Elementos");
+  printf("\n3 - Inserir Elemento");
+  printf("\n4 - Excluir Elemento");
+  printf("\n5 - Reiniciar Pilha.");
+  printf("\nDigite sua opção: ");
+  scanf("%d", &r);
+
+  return r;
+}
+
+void main()
+{
+  fila f;
+
+  iniciarFila(&f);
+
+  int r;
+
+  r = menu();
+
+  while (r != 0)
+  {
+    switch (r)
+    {
+    case 1:
+      system("clear");
+      tamanhoFila(&f);
+      break;
+    case 2:
+      system("clear");
+      exibirFila(&f);
+      break;
+    case 3:
+      system("clear");
+      inserir(&f);
+      break;
+    case 4:
+      system("clear");
+      deletar(&f);
+      break;
+    case 5:
+      system("clear");
+      resetFila(&f);
+      break;
+    }
+    r = menu();
+  }
 }
